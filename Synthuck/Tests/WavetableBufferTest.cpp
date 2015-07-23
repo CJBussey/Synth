57f951d34079a6c8c94bdd8f43ca4a54e126daa3
Carl Bussey <carl.j.bussey@gmail.com>
2015-07-23 02:20:45 +0200

    Removed empty header file.

--- Synthuck/Tests/WavetableBufferTest.cpp
+++ Synthuck/Tests/WavetableBufferTest.cpp
@@ -7,3 +7,19 @@
 //
 
 #include "WavetableBufferTest.h"
+#include "../Source/WavetableBuffer.h"
+
+int main()
+{
+    FWavetableBuffer aWavetableBuffer(10, 3);
+    
+    for (size_t nIndex = 0; nIndex < 10; ++nIndex)
+    {
+        aWavetableBuffer[nIndex] = nIndex;
+    }
+    
+    for (size_t nIndex = 0; nIndex < 10; ++nIndex)
+    {
+        std::cout << aWavetableBuffer() << std::endl;
+    }
+}
